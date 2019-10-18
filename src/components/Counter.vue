c5578b6d372b35a7e142e56a55110b1991375ddd
horrylala <horrylala@outlook.com>
2019-10-18 17:59:20 +0800

    Create Counter.vue

---
+++ src/components/Counter.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <p>当前count值是：{{ state.count }}, 三倍是: {{ state.tripple }}</p>
+  </div>
+</template>
+
+<script>
+import { createComponent, reactive, computed } from '@vue/composition-api'
+export default createComponent({
+  name: 'Counter',
+  props: {
+    count: Number
+  },
+  setup (props) {
+    const state = reactive({
+      count: computed(() => props.count),
+      tripple: computed(() => props.count * 3)
+    })
+
+    return {
+      state
+    }
+  }
+})
+</script>
+
+<style>
+
+</style>
+
