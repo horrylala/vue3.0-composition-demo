231b90b1c7d9df4a30e41b65d083e1a4f599a904
horrylala <horrylala@outlook.com>
2019-10-18 18:00:33 +0800

    Create Home.vue

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class='home'>
+    <img alt='Vue logo' src='../assets/logo.png' />
+    <Counter :count='state'/>
+    <button @click='increment'>增加</button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import { createComponent, ref } from '@vue/composition-api'
+import Counter from '@/components/Counter.vue'
+
+export default createComponent({
+  name: 'home',
+  components: {
+    Counter
+  },
+  setup () {
+    const state = ref(0)
+
+    function increment () {
+      state.value ++
+    }
+
+    return {
+      state,
+      increment
+    }
+  }
+})
+</script>
